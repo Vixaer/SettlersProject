1f64f5d66f99ae93a41a1d149f3d43236a8e7be0
Vixaer <cao@Cheeseburger-Madness.local>
2017-02-19 17:10:18 -0500

    Added Enums and Card Class

---
+++ Settlers/Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : Object {
+	
+}
+
+public class ProgressCard : Card {
+	
+}
+
+public class Stealable : Card {
+	
+}
+
+
+public class Commodity : Stealable {
+
+}
+
+public class Resource : Stealable {
+
+}
---
+++ Settlers/Assets/Scripts/Enums.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum VillageKind {Settlement,City,TradeMetropole,PoliticsMetropole,ScienceMetropole};
+enum ResourceKind {Wool,Lumber,Ore,Brick,Grain};
+enum CommodityKind {Coin,Cloth,Paper};
+enum ProgressCardKind {ArchemistCard
+	,CraneCard
+	,EngineerCard
+	,InventorCard
+	,IrrigationCard
+	,MedicineCard
+	,MiningCard
+	,PrinterCard
+	,RoadBuildingCard
+	,SmithCard
+	,BishopCard
+	,ConstitutionCard
+	,DeserterCard
+	,DiplomatCard
+	,IntrigueCard
+	,SaboteurCard
+	,SpyCard
+	,WarlordCard
+	,WeddingCard
+	,ComercialHarborCard
+	,MasterMerchantCard
+	,MerchantCard
+	,MerchantFleetCard
+	,ResourceMonopolyCard
+	,TradeMonopolyCard};
+
+enum PlayerStatus {Offline,Available,InGame,Ready};
+
+enum EventKind {Barbarian,Trade,Politics,Science};
+
+enum GamePhase {ReadyToJoin,SetupRoundOne,SetupRoundTwo,TurnFirstPhase,TurnDiceRolled,TurnSecondPhase,Completed};
+
+enum HarbourKind{None
+	,Generic
+	,Special};
+
+enum TerrainKind{Sea
+	,Desert
+	,Pasture
+	,Forest
+	,Mountains
+	,Hills
+	,Fields
+	,GoldMine};
+
+
+
+
+public class Enums : Object {}
