0a02a1764f895080740b3c2c9c4cf12f26ab90ca
Bruce Kuzak <bruce.kuzak123@gmail.com>
2017-04-11 20:04:04 -0400

    check for village explicitly in canBuildCity

--- Settlers/Assets/Scripts/Classes/Game.cs
+++ Settlers/Assets/Scripts/Classes/Game.cs
@@ -2957,7 +2957,7 @@ public class Game : NetworkBehaviour
             foreach (Intersection i in e.endPoints)
             {
 
-                if (i.owned)
+                if (i.owned &&  i.positionedUnit is Village)
                 {
                     checkProximity = false;
                     break;
